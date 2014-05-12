24f7fe9bc7edc1fb0131819b8d1df8a401997ea2
Will LaBranche <wlabranche@gmail.com>
2014-05-11 19:16:05 -0700

    Added basic functionality, will be improved

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  var parse = function(item){
+    var count = 0;
+    if (Array.isArray(item)){
+      result += '[';
+      for (var i = 0; i < item.length; i++){
+        if (count >= 1) {result += ',';}
+        parse(item[i]);
+        count++;
+      }
+      result += ']';
+    }else if (typeof item === 'string'){
+      result += '"' + item + '"';
+    }else if (item === null){
+      result += null;
+    }else if(typeof item === 'object'){
+      result += '{';
+      for (var key in item){
+        if (typeof item[key] !== 'undefined' && typeof item[key] !== 'function'){
+          if (count >= 1) {result += ',';}
+          result += '"' + key + '":';
+          parse(item[key]);
+        }
+        count++;
+      }
+      result += '}';
+    }else{
+      result += item;
+    }
+    count++;
+  };
+  parse(obj);
+  return result;
 };
